eda3d10d1ec92b50ecbf51aa1414ea8d2bac9b1b
Eldar Terzic <eldar.terzic@insum.se>
2023-01-18 19:53:16 +0100

    backend api for etin and posting transactionals

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const {port, mongoURI} = require('./config')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const { port, mongoURI } = require('./config')
+const transactionsRoutes = require('./routes/transactions')
+const path = require('path')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 mongoose.set('strictQuery', false)
 mongoose.connect(mongoURI, {
@@ -10,6 +19,6 @@ mongoose.connect(mongoURI, {
 .then(() => console.log('MongoDB is connected'))
 .catch((err) => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api/transactions', transactionsRoutes)
 
 app.listen(port, () => console.log('Express running on port ' + port))
